9f7430d899d8860cb41339015300d8849b8b4281
Frank Lai <Frank Lai@localhost>
2012-04-26 00:13:53 +0800

    init HelloWorld Package

---
+++ HelloWorldPackage/make.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# Copyright is Copyright
+#
+# icon is from
+# http://www.iconarchive.com/show/superhero-avatar-icons-by-deleket/Avengers-Iron-Man-icon.html
+
+SPK_FILE="HelloWorld.spk"
+
+make()
+{
+	if [ -f "$SPK_FILE" ]; then
+		echo "$SPK_FILE" already exists
+		exit 1
+	fi
+	
+	rm -rf _spk_dst
+	mkdir _spk_dst
+	mkdir _spk_dst/scripts
+
+	rm -rf _package_dst
+	mkdir _package_dst
+	
+	cp -af ui _package_dst/
+	echo "== Compress to package.tgz"
+	cd _package_dst/
+	tar vzcf ../_spk_dst/package.tgz *
+	cd ..
+
+	cp -af INFO _spk_dst/
+	cp -af package/scripts/* _spk_dst/scripts
+	cp -af ui/images/Avengers-Iron-Man-icon-72.png _spk_dst/PACKAGE_ICON.PNG
+
+	echo "== Compress to $SPK_FILE"
+	cd _spk_dst/
+	tar vcf ../"$SPK_FILE" *
+	cd ..
+}
+
+rm_rf()
+{
+	local path="$1"
+	
+	echo "Remove $path"
+	rm -rf "$path"
+}
+
+make_clean()
+{
+	rm_rf "$SPK_FILE"
+	rm_rf "_spk_dst"
+	rm_rf "_package_dst"
+}
+
+case $1 in
+	clean)
+		make_clean
+	;;
+	*)
+		make
+	;;
+esac
